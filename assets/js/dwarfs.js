7b10a24e427df385ed28b68c27ed3605205c7b49
AwesomePresent <yoursmile128@hotmail.com>
2019-04-10 20:38:13 -0400

    first version complete

--- assets/js/dwarfs.js
+++ assets/js/dwarfs.js
@@ -22,39 +22,39 @@ class Stats {
 var bashful = new Dwarf(
     "Bashful",
     "the shy one",
-    new Stats()
+    new Stats(80, 20, 48, 10, 20)
 );
 
 var doc = new Dwarf(
     "Doc",
     "leader of the group, not too good at speaking",
-    new Stats()
+    new Stats(150, 20, 35, 20, 20)
 );
 
 var dopey = new Dwarf(
     "Dopey",
     "annoying, silly, the comic relief",
-    new Stats()
+    new Stats(100, 10, 25, 10, 40)
 );
 var grumpy = new Dwarf(
     "Grumpy",
     "always annoyed and irritated, doesn't like snow white",
-    new Stats()
+    new Stats(120, 10, 30, 5, 10)
 );
 var happy = new Dwarf(
     "Happy",
     "fat, jolly, friendly, spreading joy to everyone",
-    new Stats()
+    new Stats(150, 15, 30, 25, 10)
 );
 var sleepy = new Dwarf(
     "Sleepy",
     "always tired, most observant",
-    new Stats()
+    new Stats(150, 10, 15, 10, 80)
 );
 var sneezy = new Dwarf(
     "Sneezy",
     "likes to have fun, has never ending case of hay ",
-    new Stats()
+    new Stats(120, 10, 15, 20, 50)
 );
 
 var dwarfs = [bashful, doc, dopey, grumpy, happy, sleepy, sneezy];
--- assets/js/game.js
+++ assets/js/game.js
@@ -2,146 +2,171 @@
 
 
 // Character Card Template
-$("document").ready(function () {
-  var characterCardTemplate = $("<div>").addClass(" card");
+// $("document").ready(function () {
+var characterCardTemplate = $("<div>").addClass(" card");
 
-  var cardImage = $("<img>").addClass("card-img-top").appendTo(characterCardTemplate);
-  var cardTitle = $("<h4>").addClass("card-title text-center mb-1").appendTo(characterCardTemplate);
-  var health = $("<h6>").addClass("card-subtitle text-center hp").appendTo(characterCardTemplate);
-  var description = $("<p>").addClass("description").appendTo(characterCardTemplate);
+var cardImage = $("<img>").addClass("card-img-top").appendTo(characterCardTemplate);
+var cardTitle = $("<h4>").addClass("card-title text-center mb-1").appendTo(characterCardTemplate);
+var health = $("<h6>").addClass("card-subtitle text-center hp").appendTo(characterCardTemplate);
+var description = $("<p>").addClass("description").appendTo(characterCardTemplate);
+var message = $("#message");
 
-  var selectedCharacter;
-  var enemyDwarfs = [];
-  var opponent;
-  var attackMultiplier = 1;
+var selectedCharacter;
+var enemyDwarfs = [];
+var opponent;
+var attackMultiplier = 1;
 
-  // rendering character selections
-  dwarfs.forEach(dwarf => {
-    health.text(dwarf.stats.hp);
-    cardTitle.text(dwarf.name);
-    description.text(dwarf.description);
-    cardImage.attr("src", dwarf.imageSrc);
+// rendering character selections
+dwarfs.forEach(dwarf => {
+  health.text(dwarf.stats.hp);
+  cardTitle.text(dwarf.name);
+  description.text(dwarf.description);
+  cardImage.attr("src", dwarf.imageSrc);
 
-    dwarf.html = characterCardTemplate.clone(true, false);
-    dwarf.html.hover(function () {
-      // console.log("hover");
-      dwarf.html.find(".description").toggleClass("show-description");
-      dwarf.html.toggleClass("player");
-      // ??? why is hover function doesn't get called after selecting character
-    });
-    $("#character-picker").append(dwarf.html);
-    // setting up for character selection event
-    dwarf.html.click(function () {
-      selectedCharacter = dwarf;
-      selectCharacter(dwarf);
-    });
+  dwarf.html = characterCardTemplate.clone(true, false);
+  dwarf.html.hover(function () {
+    dwarf.html.find(".description").toggleClass("show-description");
+    dwarf.html.toggleClass("player");
+    // ??? why is hover function doesn't get called after selecting character
   });
 
+  $("#character-picker").append(dwarf.html);
+  // setting up for character selection event
+  dwarf.html.click(function () {
+    selectCharacter(dwarf);
+  });
+});
 
 
-  function selectCharacter(char) {
-    $(".character-picker-section").empty();
-    char.html.find(".description").removeClass("show-description");
-    $("#character-selected").append(char.html)
-    for (var i = 0; i < 4; i++) {
-      var enemy = dwarfs.splice(randomNum(dwarfs.length), 1)[0];
-      if (enemy != char) {
-        enemyDwarfs.push(enemy);
-        enemy.html.addClass("enemy");
-        $("#enemy-picker").append(enemy.html);
-      }
+
+function selectCharacter(char) {
+  selectedCharacter = char;
+  $(".character-picker-section").empty(); //!!use detach() if want to keep the events with the element
+  char.html.find(".description").removeClass("show-description");
+  $("#character-selected").append(char.html);
+  for (var i = 0; i < 4; i++) {
+    var enemy = dwarfs.splice(randomNum(dwarfs.length), 1)[0];
+    if (enemy != char) {
+      enemyDwarfs.push(enemy);
+      enemy.html.addClass("enemy");
+      $("#enemy-picker").append(enemy.html);
     }
-    enemySelection();
   }
+  enemySelection();
+}
 
-  function enemySelection() {
-    enemyDwarfs.forEach(dwarf => {
-      dwarf.html.hover(function () {
-        // console.log(this);
-        $(this).toggleClass("opponent");
-      });
-      dwarf.html.click(function () {
-        opponent = dwarf;
-        $(this).remove();
-        $("#opponent").append(this);
-
-        enemyDwarfs.forEach(enemy => {
-          enemy.html.unbind('mouseenter mouseleave click');
-        });
-
-        setupFight();
-      });
+function enemySelection() {
+  enemyDwarfs.forEach(dwarf => {
+    dwarf.html.hover(function () {
+      // console.log(this);
+      $(this).toggleClass("opponent");
     });
-    $(".action-selection").addClass("hide");
-  }
+    dwarf.html.click(function () {
+      opponent = dwarf;
+      $(this).remove();
+      $("#opponent").append(this);
+
+      enemyDwarfs.forEach(enemy => {
+        enemy.html.unbind('mouseenter mouseleave click');
+      });
+
+      setupFight();
+    });
+  });
+}
 
 
-  function setupFight() {
-    $(".action-selection").removeClass("hide");
-    $("#btn-attack").click(attack)
-    $("#btn-smash").click(smash);
-  }
+function setupFight() {
+  $(".action-selection").removeClass("hide");
+  $("#btn-attack").click(attack)
+  $("#btn-smash").click(smash);
+}
 
-  // fight functions
-  function attack() {
-    var damage = (selectedCharacter.stats.attack * (attackMultiplier)) - opponent.stats.defence;
-    opponent.stats.hp -= damage;
+// fight functions
+function attack() {
+  var damage = Math.max((selectedCharacter.stats.attack * (attackMultiplier)) - opponent.stats.defence, 0);
+  opponent.stats.hp -= damage;
+  console.log("causing " + damage + " damage to opponent");
+  console.log("multiplier: " + attackMultiplier);
+  roundCalculation();
+}
+function smash() {
+  if (!dodge()) {
+    var damage = selectedCharacter.stats.attack * 2 * attackMultiplier;
+    opponent.stats.hp -= damage
+
     console.log("causing " + damage + " damage to opponent");
     console.log("multiplier: " + attackMultiplier);
-    fightResolveFunc();
   }
-  function smash() {
-    if (!dodge()) {
-      var damage = selectedCharacter.stats.attack * 2 * attackMultiplier;
-      opponent.stats.hp -= damage
+  roundCalculation();
+}
 
-      console.log("causing " + damage + " damage to opponent");
-      console.log("multiplier: " + attackMultiplier);
-    }
-    fightResolveFunc();
-  }
 
-  function fightResolveFunc() {
-    // console.log("opponent health: " + opponent.stats.hp);
-    attackMultiplier++;
-    if (!testWinCondition()) {
-      counterAttack();
-      testLoseCondition();
+// How to properly code a decision tree in turn based games?!?!
+function roundCalculation() {
+  attackMultiplier++;
+  updateUI();
+  if (enemyDead()) {
+    enemyDwarfs.splice(enemyDwarfs.indexOf(opponent), 1);
+    opponent.html.remove();
+    if (enemyDwarfs.length <= 0) {
+      endGame("Win");
+      unbindButtons();
     } else {
-      $("#btn-attack").unbind("click");
-      $("#btn-smash").unbind("click");
+      unbindButtons();
       setTimeout(function () {
-        opponent.html.remove();
         enemySelection();
-      }, 3000);
+      }, 1500);
     }
-    updateUI();
+  } else {
+    counterAttack();
+    if (characterDead()) {
+      unbindButtons();
+      endGame("Lose");
+    }
   }
+  updateUI();
+}
 
-  function dodge() {
-    return opponent.stats.dodgeChance >= randomNum(100);
-  }
+function unbindButtons() {
+  $(".action-selection").addClass("hide");
+  $("#btn-attack").unbind("click");
+  $("#btn-smash").unbind("click");
+}
 
-  function counterAttack() {
-    selectedCharacter.stats.hp -= opponent.stats.counterAttack - selectedCharacter.stats.defence
-  }
+function dodge() {
+  return opponent.stats.dodgeChance >= randomNum(100);
+}
 
-  function updateUI() {
-    $("#opponent").find(".hp").text(opponent.stats.hp);
-    $("#character-selected").find(".hp").text(selectedCharacter.stats.hp);
-  }
+function counterAttack() {
+  selectedCharacter.stats.hp -= Math.max((opponent.stats.counterAttack - selectedCharacter.stats.defence), 0)
+}
 
-  function testWinCondition() {
-    return opponent.stats.hp <= 0;
+function updateUI() {
+  $("#opponent").find(".hp").text(opponent.stats.hp);
+  $("#character-selected").find(".hp").text(selectedCharacter.stats.hp);
+}
 
-  }
+function enemyDead() {
+  return opponent.stats.hp <= 0;
 
-  function testLoseCondition() {
-    return selectedCharacter.stats.hp <= 0;
-  }
+}
 
-  // helper functions
-  function randomNum(upper) {
-    return Math.floor(Math.random() * upper);
-  }
-});
+function characterDead() {
+  return selectedCharacter.stats.hp <= 0;
+}
+
+function endGame(msg) {
+  var messageText = "Game Over! You " + msg;
+  message.text(messageText);
+  console.log(messageText);
+}
+// helper functions
+function randomNum(upper) {
+  return Math.floor(Math.random() * upper);
+}
+// });
+
+//TODOLIST
+// Refactor code
+// include proper win lose fight conditions and end game conditions
